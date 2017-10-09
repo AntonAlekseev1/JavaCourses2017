26ef423b19b1ab0aae57a46439b4975ee66809ea
Anton <anton_alekseev1993@mail.ru>
2017-10-09 22:43:46 +0300

    null

--- lesson2/task3/src/IProduct.java
+++
@@ -1,6 +0,0 @@
-
-public interface IProduct {
-public void instalFirstPart(IProductPart first);
-public void instalSecondPart(IProductPart second);
-public void instalThirdPart(IProductPart third);
-}
