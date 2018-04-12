f884ff066ef18c96d5325e1d7f59d718a954e5da
Anton <anton_alekseev1993@mail.ru>
2018-04-12 18:21:04 +0300

    diagrams

--- lesson3/task4/src/Printer.java
+++
@@ -1,6 +0,0 @@
-public class Printer {
-
-	public static void println(String string) {
-		System.out.println(string);
-	}
-}
--- lesson4/task3/runWihtArgs.bat
+++
@@ -1,3 +0,0 @@
-java -jar Hotel.jar folder/guests.txt folder/rooms.txt  folder/options.txt
-
-pause
